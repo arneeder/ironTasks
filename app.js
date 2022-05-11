d2e365d7ba7755bccadd9f8194df65b172942cc5
arneeder <arne.eder@gmail.com>
2022-05-11 09:47:21 +0200

    created and linked routes/projects.js file

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use("/api", allRoutes);
 const auth = require("./routes/auth");
 app.use("/api/auth", auth);
 
+const projects = require("./routes/projects");
+app.use("/api/projects", projects);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 

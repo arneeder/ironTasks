c2ca4c5c1e927a69243bd80793c36345f171f7c4
arneeder <arne.eder@gmail.com>
2022-05-13 11:04:13 +0200

    draggables and droppables established, not onDragEnd function yet

--- client/src/components/StatusColumn/StatusColumn.js
+++ client/src/components/StatusColumn/StatusColumn.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import './index.css'
+import { Droppable } from 'react-beautiful-dnd';
 import TaskContext from '../../context/task';
 import TaskCard from '../TaskCard/TaskCard';
 
@@ -12,7 +13,16 @@ const StatusColumn = props => {
     }, [])
 
     return (
-        <div className='staus-columns'>
+        
+        <Droppable droppableId={props.status._id}>
+        {(provided, snapshot) => (
+
+        <div className='staus-columns'
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            // isDraggingOver={snapshot.isDraggingOver}
+        >
+            
             <h3>{props.status.name}</h3>
             {
                 tasks
@@ -20,11 +30,14 @@ const StatusColumn = props => {
                         const projectStatus = task.projects.find( project => project.project === projectId)
                         return projectStatus.status === props.status._id
                     })
-                    .map( task => (
-                        <TaskCard key={task._id} task={task} />
+                    .map( (task, index) => (
+                        <TaskCard key={task._id} task={task} index={index} />
                 ))
             }
         </div>
+        )}
+        </Droppable>
+        
     )
 }
 
--- client/src/components/TaskCard/TaskCard.js
+++ client/src/components/TaskCard/TaskCard.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import './index.css'
+import { Draggable } from 'react-beautiful-dnd';
 
 const TaskCard = props => {
     return (
-        <div className="card">
-            <h4>{props.task.name}</h4>
-            <p>{props.task.description}</p>
-            {/* <div className="assigned">
-                <div className="one-assignment">
-                    <p><strong>Accountable: </strong></p>
-                    <p>{props.task.accountable}</p>
+        <Draggable draggableId={props.task._id} index={props.index}>
+            {(provided, snapshot) => (
+                <div className="card"
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                        //isDragging={snapshot.isDragging}
+                >
+                    <div>
+                        <h4>{props.task.name}</h4>
+                        <p>{props.task.description}</p>
+                        <div className="assigned">
+                            <div className="one-assignment">
+                                <p><strong>Accountable: </strong></p>
+                                <p>{props.task.accountable}</p>
+                            </div>
+                            <div className="one-assignment">
+                                <p><strong>Responsible: </strong></p>
+                                <p>{props.task.responsible}</p>
+                            </div>
+                            <div className="responsible"></div>
+                        </div>
+                    </div>
                 </div>
-                <div className="one-assignment">
-                    <p><strong>Responsible: </strong></p>
-                    <p>{props.task.responsible}</p>
-                </div>
-                <div className="responsible"></div>
-            </div> */}
-        </div>
+                )}
+        </Draggable>
     )
 }
 

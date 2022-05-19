4f810e4f31f3d77fe4c11c60a7c923520bb2d8d0
arneeder <arne.eder@gmail.com>
2022-05-19 09:01:20 +0200

    moved myprojects to projects component

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,31 +1,13 @@
 import './index.css'
-import React, { useEffect, useState } from 'react';
-import { MyProjectsContext } from '../../context/getMyProjects';
+import React from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
-import axios from 'axios';
 
-const ProjectList = () => {
-
-    const storedToken = localStorage.getItem('authToken')
-    
-    const [myProjects, setMyProjects] = useState([])
-
-    const getMyProjects = () => {
-        axios.get('/api/projects/',  { headers: { Authorization: `Bearer ${storedToken}` } } )
-            .then( myProjects => {
-              setMyProjects(() => myProjects.data)
-            })
-            .catch(error => console.log(error))
-      }
-
-
-    useEffect(() => {getMyProjects()}, [])
-    console.log({myProjects});
+const ProjectList = props => {
 
     return (
         <>
             {
-                myProjects.map( project => (
+                props.myProjects.map( project => (
                     <ProjectCard key={project._id} projectId={project._id} />
                 ))
             }
--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,20 +1,34 @@
 import './index.css'
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import ProjectCreate from '../ProjectCreate/ProjectCreate'
 import ProjectList from '../ProjectList/ProjectList'
 import Popup from '../Popup/Popup'
 import ProjectEdit from '../ProjectEdit/ProjectEdit'
 import { ProjectContext } from '../../context/getProject'
+import axios from 'axios'
 
 const Projects = () => {
 
+    const storedToken = localStorage.getItem('authToken')
     const { projectEdit, setProjectEdit } = useContext(ProjectContext)
+
+    const [myProjects, setMyProjects] = useState([])
+
+    const getMyProjects = () => {
+        axios.get('/api/projects/',  { headers: { Authorization: `Bearer ${storedToken}` } } )
+            .then( myProjects => {
+              setMyProjects(() => myProjects.data)
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {getMyProjects()}, [])
   
     return (
         <>
             <h2>Projects</h2>
             <ProjectCreate  />
-            <ProjectList  />
+            <ProjectList myProjects={myProjects}  />
             <Popup 
                 trigger={projectEdit}
                 setTrigger={setProjectEdit}
